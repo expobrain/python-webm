b0fcdbf826ee174761d69600c601fa178419cd7a
Daniele Esposti <daniele.esposti@gmail.com>
2012-03-04 16:32:48 +0000

    Reformatted source code

--- webm/tests/common.py
+++ webm/tests/common.py
@@ -34,21 +34,17 @@ import os
 import sys
 
 
-WEBP_IMAGE_FILE = os.path.join( os.path.dirname( __file__ ),
-                                "vancouver2.webp" )
-PNG_IMAGE_FILE  = os.path.join( os.path.dirname( __file__ ),
-                                "vancouver2.png" )
-DECODE_FILENAME = os.path.join( os.path.dirname( __file__ ),
-                                "decode_{0}.png" )
-ENCODE_FILENAME = os.path.join( os.path.dirname( __file__ ),
-                                "encode_{0}.webp" )
-WEBP_IMAGE_DATA = bytearray( file( WEBP_IMAGE_FILE, "rb" ).read() )
-PNG_BITMAP_DATA = bytearray( Image.open( PNG_IMAGE_FILE ).tostring() )
-IMAGE_WIDTH     = 644
-IMAGE_HEIGHT    = 484
+WEBP_IMAGE_FILE = os.path.join(os.path.dirname(__file__), "vancouver2.webp")
+PNG_IMAGE_FILE = os.path.join(os.path.dirname(__file__), "vancouver2.png")
+DECODE_FILENAME = os.path.join(os.path.dirname(__file__), "decode_{0}.png")
+ENCODE_FILENAME = os.path.join(os.path.dirname(__file__), "encode_{0}.webp")
+WEBP_IMAGE_DATA = bytearray(file(WEBP_IMAGE_FILE, "rb").read())
+PNG_BITMAP_DATA = bytearray(Image.open(PNG_IMAGE_FILE).tostring())
+IMAGE_WIDTH = 644
+IMAGE_HEIGHT = 484
 
 
-class WebPDecodeMixin( object ):
+class WebPDecodeMixin(object):
     """
     Mixin class to help WebPDecode unit tests
     """
@@ -57,18 +53,17 @@ class WebPDecodeMixin( object ):
         if sys.platform != "win32":
             from ctypes import CDLL
 
-            self.assertIsInstance( _LIBRARY, CDLL )
+            self.assertIsInstance(_LIBRARY, CDLL)
 
         else:
             raise NotImplementedError(
-                "Test non implemented under {0}".format( sys.platform )
-            )
+                "Test non implemented under {0}".format(sys.platform))
 
         # Create decoder instance
         self.webp_decoder = WebPDecoder()
 
 
-class WebPEncodeMixin( object ):
+class WebPEncodeMixin(object):
     """
     Mixin class to help WebPEncode unit tests
     """
@@ -77,12 +72,11 @@ class WebPEncodeMixin( object ):
         if sys.platform != "win32":
             from ctypes import CDLL
 
-            self.assertIsInstance( _LIBRARY, CDLL )
+            self.assertIsInstance(_LIBRARY, CDLL)
 
         else:
             raise NotImplementedError(
-                "Test non implemented under {0}".format( sys.platform )
-            )
+                "Test non implemented under {0}".format(sys.platform))
 
         # Create decoder instance
         self.webp_encoder = WebPEncoder()
